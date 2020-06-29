2796fc5b6867d80fa5ff0c60c3f67fdc81034884
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-06-29 14:05:53 +0300

    Higher order function is added

---
+++ 2-pillars-of-Js/higherOrderFunc.js
@@ -0,0 +1,2 @@
+// If a function takes another function as parameter and return function, it is higher
+// order function . It provides more dynamics and flexibility and efficiency to function workings
