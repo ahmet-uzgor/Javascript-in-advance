e51726af80486241c8b4f9a570801b0bc68e15b6
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-06-29 14:37:17 +0300

    Higher order function example is written

--- 2-pillars-of-Js/higherOrderFunc.js
+++ 2-pillars-of-Js/higherOrderFunc.js
@@ -19,3 +19,35 @@ function letAdminUser(adminName){ // for admin Users
 }
 
 // it can be increased for each category but this is very unefficient code
+// So to be able to make efficient, dynamic and flexible we will use Higher Order Func.
+
+const giveAccessTo = (person) => {
+    person.isAuthenticated = true;
+    return `Access granted to${person.name}`;
+}
+
+function authenticate(person){
+    let array = [];
+    for (let i =0 ; i < person.authenticationTime; i++){
+        array.push(i);
+    }
+    return giveAccessTo(person);
+}
+
+function letPerson(person, fn){
+    if (person.level === 'admin' && person.isAuthenticated === true){ // it can do via authentication
+        return fn(person);
+    }else if (person.level === 'user' && person.isAuthenticated === true){
+        return fn(person);
+    }else {
+        return `${person.name} is failed`
+    }
+
+}
+
+function IncreaseSalary(person){
+    return ` ${person.name} salary is increased`;
+}
+
+let person1 = {level: 'user', name: 'Ahmet', isAuthenticated: false}
+letPerson(person1, IncreaseSalary); // it authenticates and if authentication is successfull it Increase Salary
