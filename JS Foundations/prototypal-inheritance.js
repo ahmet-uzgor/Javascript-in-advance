f6e43f3c4bad5511c1b552ca115ae292bbaf4d34
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-07-02 01:35:31 +0300

    Prototypal chain example is added

--- JS Foundations/prototypal-inheritance.js
+++ JS Foundations/prototypal-inheritance.js
@@ -11,3 +11,27 @@ a.__proto__.__proto__ ; // It gives prototypal features of its master (Objects)
 /**
  * Example of prototypal inheritance
  */
+let dragon = {
+    name: "Summo",
+    fire: true,
+    fight(){
+        return 5
+    },
+    sing() {
+        if (this.fire){
+            return `I am ${this.name} , the breather of fire`
+        }
+    }
+}
+
+let lizard = {
+    name: 'Ahmet',
+    fight (){
+        return 1;
+    }
+}
+
+lizard.__proto__ = dragon; // It adds feature of dragon to prototypal chain for lizard
+lizard.sing(); // If lizard has not sing func. it looks for prototypal chain and it goes to up
+// and it will find sing() into dragon
+dragon.isPrototypeOf(lizard); // It returns answer of Is dragon prototype of lizard .
