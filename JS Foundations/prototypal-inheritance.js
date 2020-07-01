770ccd3067bd990fa39f5d2717105107c5c6d212
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-07-02 01:41:59 +0300

    hasOwnProperty example is written

--- JS Foundations/prototypal-inheritance.js
+++ JS Foundations/prototypal-inheritance.js
@@ -35,3 +35,17 @@ lizard.__proto__ = dragon; // It adds feature of dragon to prototypal chain for 
 lizard.sing(); // If lizard has not sing func. it looks for prototypal chain and it goes to up
 // and it will find sing() into dragon
 dragon.isPrototypeOf(lizard); // It returns answer of Is dragon prototype of lizard .
+
+
+// Property checking for objects
+
+for (let prop in lizard){
+    console.log(prop); // It lists all properties of lizard
+}
+
+// Also hasOwnProperty method
+for (let prop in lizard){
+    if (lizard.hasOwnProperty(prop)){ // it checks lizard has own property as given property
+        console.log(prop);
+    }
+}
