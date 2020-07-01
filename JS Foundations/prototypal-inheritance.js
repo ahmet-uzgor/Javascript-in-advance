2b058bdb2506d691e644aaa3186d491cbad90aab
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-07-02 01:27:15 +0300

    Prototypal inheritance is created

---
+++ JS Foundations/prototypal-inheritance.js
@@ -0,0 +1,13 @@
+// It includes __proto__ , Prototypal chains , Protoypal Inheritance subjects
+
+// Arrays -- Functions are also objects and take features from object as prototypal inheritted
+const a = []; 
+a.__proto__ ; // it gives whole prototypal features od array 
+a.__proto__.__proto__ ; // It gives prototypal features of its master (Objects)
+
+// It is same as function 
+
+
+/**
+ * Example of prototypal inheritance
+ */
