c70e400df49e0e9f0c45f643c009df77a6959ea0
ahmet-uzgor <ahmetuzgor10@gmail.com>
2020-07-09 13:57:46 +0300

    try-catch-finally example is added

--- Error-handling/try-catch-finally.js
+++ Error-handling/try-catch-finally.js
@@ -1,1 +1,14 @@
 // It includes example of try-catch-finally error handling
+function fail(){
+    try {
+        console.log('It succesfully worked');
+        // throw new Error(''ooops) sample error throwing
+    } catch (error){
+        console.log(error.message)
+    } finally {
+        console.log('Finally block worked');
+        return 'returning finally'
+    }
+}
+
+fail();
